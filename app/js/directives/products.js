5019ad99f3d1cb92ec4dc88835e1b84abe5ce5dd
Agusteen <agustin.php@gmail.com>
2016-11-26 15:41:48 -0300

    Proyecto agregado

---
+++ app/js/directives/products.js
@@ -0,0 +1,33 @@
+'use strict';
+var app = angular.module('storeProducts', ['ngRoute']).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+	$locationProvider.hashPrefix('!');	
+}]);
+
+app.directive('productTitle', function(){
+	return {
+		restrict: 'A',
+		templateUrl: 'partials/product-title.html'		
+	};
+});
+
+app.directive('productPanels', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'partials/product-panels.html',
+		controller: function(){
+			this.tab = 1;
+
+			this.selectTab = function (setTab){
+				this.tab = setTab;		
+			};
+
+			this.isSelected = function(checkTab){		
+				return this.tab === checkTab;
+			};
+		},
+		controllerAs: 'panel'
+	};
+});
+
+
